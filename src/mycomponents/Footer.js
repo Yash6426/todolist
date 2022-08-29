d43854f68c724efb7081253b60e96294a983cfa7
yash <yashyashkumar1654@gmail.com>
2022-08-29 17:54:50 +0530

    react

---
+++ src/mycomponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer class="bg-dark text-light py-3">
+            <p class="text-center">
+            Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    )
+}
